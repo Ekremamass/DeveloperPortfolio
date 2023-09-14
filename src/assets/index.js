74ae32e726a4c5758343c923aa7da311eb61d61b
Ekremamass <Ekremamass@gmail.com>
2023-09-14 15:50:11 +0300

    J

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,7 +21,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import java from "./tech/java.png";
+import java from "./tech/Java.png";
 
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
